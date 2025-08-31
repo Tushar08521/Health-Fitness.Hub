6c67161c57b5aeedcd2c7aa2d8f8d130eb5c99fd
Tushar08521 <159631282+Tushar08521@users.noreply.github.com>
2025-08-31 10:41:50 +0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,20 @@
 // Tailwind configuration
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        greenPrimary: "#1e7e34",
+        greenLight: "#d4edda",
+      },
+      borderRadius: {
+        "2xl": "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
