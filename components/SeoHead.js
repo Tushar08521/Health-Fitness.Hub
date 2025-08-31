c22ebdcc83892b12687e48961bd975df0c8b37a7
Tushar08521 <159631282+Tushar08521@users.noreply.github.com>
2025-08-31 10:12:57 +0600

    SeoHead.js

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -1,2 +1,26 @@
 // SEO meta tags
 // Replace title, description, url, image per page
+export default function SeoHead({ title, description, url, image, keywords }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />   {/* Add keywords here */}
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href={url} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image || "/default-image.jpg"} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image || "/default-image.jpg"} />
+    </Head>
+  );
+}
