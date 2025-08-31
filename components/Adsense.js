6dd7692fa722bb51381721ed6f4c8960a7500ce0
Tushar08521 <159631282+Tushar08521@users.noreply.github.com>
2025-08-31 10:30:35 +0600

    Adsense.js

--- components/Adsense.js
+++ components/Adsense.js
@@ -1,2 +1,16 @@
 // Google AdSense script
 // Replace Publisher ID in page ad units
+import Script from "next/script";
+
+export default function Adsense() {
+  return (
+    <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
