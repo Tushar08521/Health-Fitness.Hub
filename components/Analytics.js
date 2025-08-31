fb8032c877c16dc95c697354bec2819bfb2776ca
Tushar08521 <159631282+Tushar08521@users.noreply.github.com>
2025-08-31 10:30:11 +0600

    Analytics.js

--- components/Analytics.js
+++ components/Analytics.js
@@ -1,2 +1,24 @@
 // GA4 tracking
 // Replace with your GA4 ID if needed
+import Script from "next/script";
+
+export default function Analytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-503116167"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-503116167', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
